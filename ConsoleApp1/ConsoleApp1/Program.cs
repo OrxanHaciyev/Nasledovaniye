8e9d54a5fb7a7f456f83fb669d03f476204b3688
orxan <pointblank_orxan@mail.ru>
2020-09-11 00:30:55 +0300

    sozdal

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bill num = new Bill(500, 456546546, "07,08,1996");
+            num.PrintSumm();
+            num.PrintData();
+            FiziLico num1 = new FiziLico(500,24242424,"05,03,2010","depozit");
+            num1.PrintData();
+            num1.Procent();
+            num1.PrintSumm();
+        }
+       
+    }
+
+    public class Bill
+    {
+
+        protected int Summ { get; set; }
+        protected int Billnumber { get; set; }
+        protected string Data { get; set; }
+        public Bill()
+        {
+
+        }
+        public Bill(int summ, int billnumber, string data)
+        {
+            this.Summ = summ; this.Billnumber = billnumber; this.Data = data;
+        }
+        
+        public void PrintSumm()
+        {
+            Console.WriteLine($"сумма на счету {Summ}");
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"дата создания счета {Data}");
+        }
+    }
+        public class FiziLico : Bill
+        {
+            string Vid { get; set; }
+            public FiziLico()
+            {
+
+            }
+            public FiziLico(int summ, int billnumber, string data, string vid) : base(summ, billnumber, data)
+            {
+                
+            }
+            public void Procent()
+            {
+                Summ =Summ+ 100;//начисляет+100$
+            }
+            public void Snatiye()
+            {
+                Summ = Summ+ 100;//снимает деньги
+            }
+        }
+        public class YurLico : Bill
+        {
+             public YurLico(int summ, int billnumber, string data, string vid) : base(summ, billnumber, data)
+             {
+
+             }
+              public  void Procent()
+              {
+                 Summ = Summ + 100;//начисляет+100$
+              }
+        }
+        
+
+        
+    
+
+}
---
+++ ConsoleAppNasledovanie_1/ConsoleAppNasledovanie_1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleAppNasledovanie_1
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student("Axmedov F.", 2, 0519);
+            st1.Print();
+            Aspirant asp1 = new Aspirant("Haciyev O.",3, 456, "тема десертации");
+            asp1.Print();
+            Aspirant asp2 = new Aspirant(st1.Lastname,st1.Kurs,st1.Zacetka,"тема десертации");
+            asp2.Print();
+
+        }
+    }
+    public class Student
+    {
+        private string lastname;
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        private int kurs;
+        public int Kurs
+        {
+            get { return kurs; }
+            set { kurs = value; }
+
+        }
+        private int zacetka;
+        public int Zacetka
+        {
+            get { return zacetka; }
+            set { zacetka = value; }
+        }
+        public Student()
+        {
+
+        }
+        public Student(string lastname, int kurs, int zacetka)
+        {
+            this.Lastname = lastname; this.Kurs = kurs; this.Zacetka = zacetka;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"студент {Lastname} на {Kurs} курсе номер зачетной книжки {Zacetka}");
+        }
+    }
+    public class Aspirant : Student
+    {
+        private string tema;
+        public Aspirant()
+        {
+
+        }
+        public Aspirant(string lastname, int kurs, int zacetka,string tema) : base(lastname, kurs, zacetka)
+        {
+            this.Tema = tema;
+        }
+        public string Tema
+        {
+            get { return tema; }
+            set { tema = value; }
+        }
+        public new void Print()
+        {
+            
+            base.Print();
+            Console.WriteLine($"тема десертации : {Tema} ");
+        }
+
+    }
+    
+
+    
+}
